2b2cac3b42767714b53c533cfa55d1efde3ff738
rajstormo <rajstormo@gmail.com>
2023-04-23 01:10:24 +0530

    added initial files

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const app = express()
+
+app.get("/",(req,res) => {
+	res.end("Index page");
+})
+
+app.get("/about",(req,res) => {
+	res.end("About page");
+})
+
+app.listen(4000, () => console.log("listening on port 3000"));
